fe6afa30ab80dcf9601e045719e82a3f232dac8a
Siphiwo10 <siphiwoj10@gmail.com>
2023-09-25 12:47:12 -0700

    fix

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,23 +1,29 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
-* _strcat- function that appends the src to dest for overwriting
-* @dest: destination of the original pointer src
-* @src: original pointer of the string
-* Return: dest
-*/
+ * _strcat - concatenates two strings
+ * @dest: input value
+ * @src: input value
+ *
+ * Return: void
+ */
 char *_strcat(char *dest, char *src)
 {
-	int destlen = 0; 
-	int srclen = 0; 
-	int a;
+	int i;
+	int j;
 
-	for (a = 0; dest[a] != '\0'; a++)
-		destlen++;
-	for (a = 0; src[a] != '\0'; a++)
-		srclen++;
-	for (a = 0; a <= srclen; a++)
-		dest[destlen + a] = src[a];
-return (dest);
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	dest[i] = '\0';
+	return (dest);
 }
